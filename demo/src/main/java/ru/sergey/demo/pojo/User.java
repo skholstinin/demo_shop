bbd333c705b9b598a09c510f55792cc5ff3bc432
sergey <s.kholstinin@yandex.ru>
2020-03-17 01:07:56 +0300

    Initial commit

---
+++ demo/src/main/java/ru/sergey/demo/pojo/User.java
@@ -0,0 +1,129 @@
+package ru.sergey.demo.pojo;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    private int id;
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private String role;
+    private boolean isEnabled;
+    private List<RoleAction> roleActions = new ArrayList<>();
+
+    public User(String name,
+                String surname,
+                String login,
+                String password,
+                String role,
+                boolean isEnabled) {
+
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.isEnabled = isEnabled;
+    }
+
+    public User() {
+    }
+
+    public User(String name, String surname, String login, String password, String role, boolean isEnabled, List<RoleAction> roleActions) {
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.isEnabled = isEnabled;
+        this.roleActions = roleActions;
+    }
+
+    public void setRoleActions(List<RoleAction> roleActions) {
+        this.roleActions = roleActions;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "surname")
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "role")
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Column(name = "is_enabled")
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "role", referencedColumnName = "role", updatable = false, insertable = false)
+    public List<RoleAction> getRoleActions() {
+        if (roleActions == null) {
+            return new ArrayList<>();
+        }
+        return Collections.unmodifiableList(roleActions);
+    }
+
+}
